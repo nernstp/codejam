64c559c615bad7aa7f876c97c7c6f9fad76f2e90
nernstp <ymwsimon@gmail.com>
2020-03-20 16:57:37 +0800

    a

---
+++ 2019/QR/Q1/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+    int n;
+    cin>>n;
+    string a,b;
+    
+    
+    }
+        
+    
+    
+    return 0;
+}
+    
