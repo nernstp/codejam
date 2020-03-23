3ec4887b67504337400f677c676b1d23b93fbdc1
nernstp <ymwsimon@gmail.com>
2020-03-23 14:21:49 +0800

    Update a.cpp

--- 2019/QR/Q1/a.cpp
+++ 2019/QR/Q1/a.cpp
@@ -4,15 +4,20 @@ int main(){
     int t;
     cin>>t;
     for(int i=1;i<=t;i++){
-    int n;
-    cin>>n;
-    string a,b;
-    
-    
+        string a;
+        string b="";
+        cin>>a;
+        for(int j=0;j<a.length();j++){
+            if(a[i]=='4'){
+                a[i]='2';
+                b++"2";
+            }
+            else
+                if(!b.empty())
+                    b++"0";
+        }
+        printf("Case #%i: %s %s",i, a, b);
     }
-        
-    
-    
     return 0;
 }
     
