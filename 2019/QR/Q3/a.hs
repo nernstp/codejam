33377646e83c0adfe48da0f6fefd6a3874d1a08d
nernstp <ymwsimon@gmail.com>
2020-03-24 17:05:54 +0800

    a

--- 2019/QR/Q3/a.hs
+++ 2019/QR/Q3/a.hs
@@ -1,7 +1,23 @@
 import Control.Monad
-
+import Data.List
+import Data.Maybe
 main = do
-            t <- readLn
-            forM_ [1..t] (\i -> 
+        t <- readLn
+        forM_ [1..t] 
+            (\i-> do   
+                n<- getLine
+                --l<- getLine
+                list<- fmap (fmap read . words) getLine
+                let tlist = zipWith gcd list $ tail list in
+                    let ntlist = (div (head list) (head tlist)) : tlist ++ [div (last list) (last tlist)]  in
+                        let m = flip zip ['A'..] $ nub $ sort ntlist in
+                        do
+                            putStr $ "Case #"++ show i++": "
+                            putStrLn $ fromJust $ sequence $ map (flip lookup m) ntlist
+                        
+                            
+                )
+                
+                
         
         
