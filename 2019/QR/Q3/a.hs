f04c0ad5dc2e92d2dfeb144263ccbc50780276e7
nernstp <ymwsimon@gmail.com>
2020-03-29 22:14:11 +0800

    a

--- 2019/QR/Q3/a.hs
+++ 2019/QR/Q3/a.hs
@@ -1,6 +1,7 @@
 import Control.Monad
 import Data.List
 import Data.Maybe
+import Control.Applicative
 main = do
         t <- readLn
         forM_ [1..t] 
@@ -8,16 +9,25 @@ main = do
                 n<- getLine
                 --l<- getLine
                 list<- fmap (fmap read . words) getLine
-                let tlist = zipWith gcd list $ tail list in
-                    let ntlist = (div (head list) (head tlist)) : tlist ++ [div (last list) (last tlist)]  in
-                        let m = flip zip ['A'..] $ nub $ sort ntlist in
-                        do
-                            putStr $ "Case #"++ show i++": "
-                            putStrLn $ fromJust $ sequence $ map (flip lookup m) ntlist
+                let dlist = map head $ group list in
+                    let start = gcd (head dlist) $ head $ tail dlist in
+                       let fact = fromJust $ (sequence $ scanl (\a b-> a>>=(\y->justDiv b y)) (Just (div (head dlist) start)) list)  <|>(sequence $ scanl (\a b-> a>>=(\y->justDiv b y)) (Just start) list) in
+                            let slist =  flip zip ['A'..] $ nub $ sort fact in
+                                do
+                                    putStr $ "Case #"++ show i++": "
+                                    putStrLn $ fromJust $ sequence $ map (flip lookup slist) fact
+                                    -- putStrLn $ fromJust $ map (flip lookup slist) fact
+                
+                -- let tlist = zipWith gcd list $ tail list in
+                    -- let ntlist = (div (head list) (head tlist)) : tlist ++ [div (last list) (last tlist)]  in
+                        -- let m = flip zip ['A'..] $ nub $ sort ntlist in
+                        -- do
+                            -- putStr $ "Case #"++ show i++": "
+                            -- putStrLn $ fromJust $ sequence $ map (flip lookup m) ntlist
                         
                             
                 )
                 
-                
+justDiv a b = if (mod a b)==0 then Just $ div a b else Nothing                
         
         
