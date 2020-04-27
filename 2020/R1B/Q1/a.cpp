645962f0bee2658bea5bccdc81fb65ecb94fa8ae
nernstp <ymwsimon@gmail.com>
2020-04-27 19:19:31 +0800

    Create a.cpp

---
+++ 2020/R1B/Q1/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        long long x,y;
+        cin>>x,yl
+        vector<long long> step;
+        
+        
+    }
+    
+    
+    return 0;
+}
